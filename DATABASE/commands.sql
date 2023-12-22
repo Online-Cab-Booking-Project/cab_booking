e0098590bad85f7cb3e5ae1240b30482bdb56306
Kshitij-V-80352 <kvanarase1@gmail.com>
2023-12-22 23:11:57 +0530

    database files added

---
+++ DATABASE/commands.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE IF NOT EXISTS opulent;
+USE opulent;
+
+CREATE TABLE wallets (
+	wallet_id INT AUTO_INCREMENT PRIMARY KEY,
+	wallet_balance DOUBLE
+);
+
+CREATE TABLE passengers (
+	p_id INT AUTO_INCREMENT PRIMARY KEY,
+	p_name_first VARCHAR(20) NOT NULL,
+	p_name_last VARCHAR(20) NOT NULL,
+	p_email VARCHAR(255) NOT NULL UNIQUE,
+	p_address TEXT(255),
+	p_dob DATE,
+	p_gender CHAR(1),
+	p_wallet_id INT,
+	CONSTRAINT FK_passengerwallet
+	FOREIGN KEY (p_wallet_id)
+	REFERENCES wallets(wallet_id)
+	ON DELETE CASCADE
+); 
+	
+DELIMITER //
+CREATE TRIGGER after_insert_passenger
+BEFORE INSERT
+ON passengers FOR EACH ROW
+BEGIN
+  INSERT INTO wallets (wallet_balance) VALUES (0.0);
+  SET NEW.p_wallet_id = LAST_INSERT_ID();
+END;
+//
+DELIMITER ;
+	
+
---
+++ DATABASE/insert_commands.sql
@@ -0,0 +1,31 @@
+INSERT INTO passengers (p_name_first,p_name_last,p_email,p_address,p_dob,p_gender)
+VALUES (
+	'Kshitij',
+	'Vanarase',
+	'kvanarase1@gmail.com',
+	'NT2-1401, The Crown Green Condominiums, Near Infosys, Hinjewadi-Phase 2, Hinjewadi, Pune.',
+	19980408,
+	'M') ,
+	(
+	'Shriyash',
+	'Band',
+	'shriyashband@gmail.com',
+	'NT2-1401, The Crown Green Condominiums, Near Infosys, Hinjewadi-Phase 2, Hinjewadi, Pune.',
+	19980408,
+	'M') ,
+	(
+	'Niraj',
+	'Zambre',
+	'nirajzambre16@gmail.com',
+	'T2-805, The Crown Green Condominiums, Near Infosys, Hinjewadi-Phase 2, Hinjewadi, Pune.',
+	19980408,
+	'M') ,
+	(
+	'Saurabh',
+	'Niley',
+	'saurabh.niley@gmail.com',
+	'A-506, The New Society, IDK nagar, Chandrapur(W)',
+	19980408,
+	'M')
+;
+	
