61b24ae51c2fe98bee68d5952201163420cc1bbb
Kshitij-V-80352 <kvanarase1@gmail.com>
2024-02-22 02:14:29 +0530

    DTO update and dummy data queries written

--- CODE/BACK_END/OpulentCabsBackend/src/main/java/site/opcab/dto/PassengerDTO.java
+++ CODE/BACK_END/OpulentCabsBackend/src/main/java/site/opcab/dto/PassengerDTO.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import site.opcab.entities.PassengerWallet;
 
 public class PassengerDTO {
+	@JsonProperty(access = Access.READ_ONLY)
 	private Long id;
 	private String firstName;
 	private String lastName;
--- CODE/BACK_END/OpulentCabsBackend/src/main/java/site/opcab/dto/PassengerWalletDTO.java
+++ CODE/BACK_END/OpulentCabsBackend/src/main/java/site/opcab/dto/PassengerWalletDTO.java
@@ -1,25 +1,12 @@
 package site.opcab.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
-
-import site.opcab.entities.Passenger;
-
 public class PassengerWalletDTO {
 
 	private Integer walletId;
 	private Double balance;
-	@JsonProperty(access = Access.WRITE_ONLY)
-	private Passenger passenger;
 
 	public PassengerWalletDTO() {
 		// TODO Auto-generated constructor stub
-	}
-
-	public PassengerWalletDTO(Integer walletId, Double balance, Passenger passenger) {
-		this.walletId = walletId;
-		this.balance = balance;
-		this.passenger = passenger;
 	}
 
 	public Integer getWalletId() {
@@ -36,14 +23,6 @@ public class PassengerWalletDTO {
 
 	public void setBalance(Double balance) {
 		this.balance = balance;
-	}
-
-	public Passenger getPassenger() {
-		return passenger;
-	}
-
-	public void setPassenger(Passenger passenger) {
-		this.passenger = passenger;
 	}
 
 }
