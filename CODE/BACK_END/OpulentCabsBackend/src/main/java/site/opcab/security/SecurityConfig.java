1f8c49c65ccd0fb9b85ab23d5b3737d5d40d6877
SHRIYASH-BAND <shriyashband@gmail.com>
2024-03-03 23:56:00 +0530

    removing permitAll extra paths

--- CODE/BACK_END/OpulentCabsBackend/src/main/java/site/opcab/security/SecurityConfig.java
+++ CODE/BACK_END/OpulentCabsBackend/src/main/java/site/opcab/security/SecurityConfig.java
@@ -33,11 +33,9 @@ public class SecurityConfig {
 		http.
 		// disable CSRF token generation n verification
 				csrf().disable().cors().and().authorizeRequests()
-				.antMatchers("/passenger/register", "/passenger/login", "/driver/register", "/driver/login",
-						"/passenger/bookride", "/passenger/bookride/getanswer",
-						"/passenger/bookride/updatebookingstatus")
-				.permitAll().antMatchers("/passenger").hasRole("P").antMatchers("/driver").hasRole("D").anyRequest()
-				.authenticated().and().httpBasic().and()
+				.antMatchers("/passenger/register", "/passenger/login", "/driver/register", "/driver/login").permitAll()
+				.antMatchers("/passenger").hasRole("P").antMatchers("/driver").hasRole("D").anyRequest().authenticated()
+				.and().httpBasic().and()
 
 				// to tell spring sec : not to use HttpSession to store user's auth details
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
