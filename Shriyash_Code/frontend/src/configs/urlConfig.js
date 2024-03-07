618c3fb19fe8969b8305990970fe04028eaceffd
SHRIYASH-BAND <shriyashband@gmail.com>
2024-03-07 08:46:55 +0530

    url changed to localhost

--- Shriyash_Code/frontend/src/configs/urlConfig.js
+++ Shriyash_Code/frontend/src/configs/urlConfig.js
@@ -1,3 +1,3 @@
-var url = "http://13.232.154.234:8080";
-export var graphURL = "http://13.232.154.234:7070"
+var url = "http://localhost:8080";
+export var graphURL = "http://localhost:7070"
 export default url;
